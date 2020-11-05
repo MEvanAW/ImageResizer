726d48fcb02d2b10df0465f350e0ce72347f76fb
MEvanAW <evananindya@mail.ugm.ac.id>
2020-11-05 18:22:06 +0700

    Membuat file picker dan menampilkan gambar beserta nama gambar

--- ImageResizer/ImageResizerForm.Designer.cs
+++ ImageResizer/ImageResizerForm.Designer.cs
@@ -33,11 +33,12 @@
             this.namaGambarLabel = new System.Windows.Forms.Label();
             this.pilihGambarButton = new System.Windows.Forms.Button();
             this.ukuranBerkasLabel = new System.Windows.Forms.Label();
-            this.ukuranTextBox = new System.Windows.Forms.TextBox();
             this.perkecilUkuranMenjadiLabel = new System.Windows.Forms.Label();
             this.lakukanButton = new System.Windows.Forms.Button();
             this.downloadHasilPadaLinkIniLabel = new System.Windows.Forms.Label();
             this.downloadLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,6 +47,7 @@
             this.pictureBox.Location = new System.Drawing.Point(12, 12);
             this.pictureBox.Name = "pictureBox";
             this.pictureBox.Size = new System.Drawing.Size(640, 360);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             // 
@@ -66,6 +68,7 @@
             this.pilihGambarButton.TabIndex = 3;
             this.pilihGambarButton.Text = "Pilih Gambar";
             this.pilihGambarButton.UseVisualStyleBackColor = true;
+            this.pilihGambarButton.Click += new System.EventHandler(this.pilihGambarButton_Click);
             // 
             // ukuranBerkasLabel
             // 
@@ -76,21 +79,14 @@
             this.ukuranBerkasLabel.TabIndex = 4;
             this.ukuranBerkasLabel.Text = "Ukuran Berkas:";
             // 
-            // ukuranTextBox
-            // 
-            this.ukuranTextBox.Location = new System.Drawing.Point(560, 421);
-            this.ukuranTextBox.Name = "ukuranTextBox";
-            this.ukuranTextBox.Size = new System.Drawing.Size(92, 26);
-            this.ukuranTextBox.TabIndex = 5;
-            // 
             // perkecilUkuranMenjadiLabel
             // 
             this.perkecilUkuranMenjadiLabel.AutoSize = true;
             this.perkecilUkuranMenjadiLabel.Location = new System.Drawing.Point(336, 424);
             this.perkecilUkuranMenjadiLabel.Name = "perkecilUkuranMenjadiLabel";
-            this.perkecilUkuranMenjadiLabel.Size = new System.Drawing.Size(218, 20);
+            this.perkecilUkuranMenjadiLabel.Size = new System.Drawing.Size(180, 20);
             this.perkecilUkuranMenjadiLabel.TabIndex = 6;
-            this.perkecilUkuranMenjadiLabel.Text = "Perkecil ukuran menjadi (MB):";
+            this.perkecilUkuranMenjadiLabel.Text = "Perkecil ukuran menjadi:";
             // 
             // lakukanButton
             // 
@@ -120,20 +116,35 @@
             this.downloadLinkLabel.TabStop = true;
             this.downloadLinkLabel.Text = "link download";
             // 
+            // comboBox
+            // 
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(522, 421);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(128, 28);
+            this.comboBox.TabIndex = 10;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "Berkas Gambar|*.jpg;*.jpeg;*.png";
+            // 
             // ImageResizerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(662, 544);
+            this.Controls.Add(this.comboBox);
             this.Controls.Add(this.downloadLinkLabel);
             this.Controls.Add(this.downloadHasilPadaLinkIniLabel);
             this.Controls.Add(this.lakukanButton);
             this.Controls.Add(this.perkecilUkuranMenjadiLabel);
-            this.Controls.Add(this.ukuranTextBox);
             this.Controls.Add(this.ukuranBerkasLabel);
             this.Controls.Add(this.pilihGambarButton);
             this.Controls.Add(this.namaGambarLabel);
             this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ImageResizerForm";
             this.Text = "Image Resizer";
@@ -150,11 +161,12 @@
         private System.Windows.Forms.Label namaGambarLabel;
         private System.Windows.Forms.Button pilihGambarButton;
         private System.Windows.Forms.Label ukuranBerkasLabel;
-        private System.Windows.Forms.TextBox ukuranTextBox;
         private System.Windows.Forms.Label perkecilUkuranMenjadiLabel;
         private System.Windows.Forms.Button lakukanButton;
         private System.Windows.Forms.Label downloadHasilPadaLinkIniLabel;
         private System.Windows.Forms.LinkLabel downloadLinkLabel;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
 
--- ImageResizer/ImageResizerForm.cs
+++ ImageResizer/ImageResizerForm.cs
@@ -15,11 +15,26 @@ namespace ImageResizer
         public ImageResizerForm()
         {
             InitializeComponent();
+            comboBox.Items.AddRange(new object[] { "90%",
+            "75%",
+            "50%",
+            "25%",
+            "10%"});
+            comboBox.SelectedIndex = 0;
         }
 
         private void ImageResizerForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void pilihGambarButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Load(openFileDialog.FileName);
+                namaGambarLabel.Text = openFileDialog.FileName;
+            }
         }
     }
 }
